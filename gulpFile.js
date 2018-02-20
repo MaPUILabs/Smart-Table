b0cb8823ee65f87320421345851df4ac15473ba2
victor <pigeonvictor@gmail.com>
2018-02-20 10:31:39 +0100

    add onItemsChanged callback

--- gulpFile.js
+++ gulpFile.js
@@ -2,38 +2,29 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var karma = require('karma').server;
-var jshint = require('gulp-jshint');
 var insert = require('gulp-insert');
 var sourcemaps = require('gulp-sourcemaps');
-var stylish = require('jshint-stylish');
 var packageJson = require('./package.json');
 var pluginList = ['stSearch', 'stSelectRow', 'stSort', 'stPagination', 'stPipe'];
 var disFolder = './dist/';
 var src = (['smart-table.module', 'stConfig', 'stTable']).concat(pluginList).map(function (val) {
-    return 'src/' + val + '.js';
+   return 'src/' + val + '.js';
 });
 
 src.push('src/bottom.txt');
 src.unshift('src/top.txt');
 
-//just as indication
-gulp.task('lint', function () {
-    gulp.src(src)
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish));
-});
-
 
 gulp.task('karma-CI', function (done) {
-    var conf = require('./test/karma.common.js');
-    conf.singleRun = true;
-    conf.browsers = ['PhantomJS'];
-    conf.basePath = './';
-    karma.start(conf, done);
+   var conf = require('./test/karma.common.js');
+   conf.singleRun = true;
+   conf.browsers = ['PhantomJS'];
+   conf.basePath = './';
+   karma.start(conf, done);
 });
 
 gulp.task('uglify', function () {
-    gulp.src(src)
+   gulp.src(src)
       .pipe(concat('smart-table.min.js'))
       .pipe(sourcemaps.init())
       .pipe(uglify())
@@ -42,19 +33,19 @@ gulp.task('uglify', function () {
 });
 
 gulp.task('concat', function () {
-    gulp.src(src, { base: '.' })
+   gulp.src(src, { base: '.' })
       .pipe(concat('smart-table.js'))
       .pipe(gulp.dest(disFolder));
 });
 
 gulp.task('test', ['karma-CI']);
 
-gulp.task('build',['uglify', 'concat'], function () {
+gulp.task('build',['test', 'uglify', 'concat'], function () {
 
-    var version = packageJson.version;
-    var string = '/** \n* @version ' + version + '\n* @license MIT\n*/\n';
+   var version = packageJson.version;
+   var string = '/** \n* @version ' + version + '\n* @license MIT\n*/\n';
 
-    gulp.src(disFolder + '*.js')
-        .pipe(insert.prepend(string))
-        .pipe(gulp.dest(disFolder));
+   gulp.src(disFolder + '*.js')
+      .pipe(insert.prepend(string))
+      .pipe(gulp.dest(disFolder));
 });
